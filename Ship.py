56c6a9bbd70f219dbffb7909e50bd519d4e7c6a7
CWARNER9 <m266696@usna.edu>
2023-11-14 09:36:21 -0500

    Ship

---
+++ Ship.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+
+class SpaceShip(pygame.sprite.Sprite):
+    def __init__(self,screen):
+        super().__init__()
+        self.image = pygame.image.load('Assets/Images/spaceShips_009.png')
+        self.image = pygame.transform.scale_by(self.image, 0.5)
+        self.image = pygame.transform.flip(self.image, False, True)
+        self.rect = self.image.get_rect()
+        self.rect.x = screen.get_width()/2
+        self.rect.y = 600
+        self.velocity = -1
+
+
+    def update(self):
+        self.rect.x += self.velocity
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
--- Space Fighterz.py
+++ Space Fighterz.py
@@ -1,5 +1,6 @@
 import pygame
 from random import randint
+from Ship import SpaceShip
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -18,11 +19,31 @@ for r in range(num_rocks):
 
 screen.blit(background, (0,0))
 
+my_ship = SpaceShip(screen)
+ship_group = pygame.sprite.Group()
+ship_group.add(my_ship)
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                my_ship.velocity += 3
+            if event.key == pygame.K_LEFT:
+                my_ship.velocity -= 3
+            if my_ship.velocity >= 3:
+                my_ship.velocity = 3
+            if my_ship.velocity <= -3:
+                my_ship.velocity = -3
+            if my_ship.rect.x == 800:
+                my_ship.velocity = 0
+            if my_ship.rect.x == 200:
+                my_ship.velocity = 0
+    ship_group.update()
     screen.blit(background, (0, 0))
-    clock.tick()
+    ship_group.draw(screen)
+    clock.tick(60)
     pygame.display.flip()
+pygame.quit()
